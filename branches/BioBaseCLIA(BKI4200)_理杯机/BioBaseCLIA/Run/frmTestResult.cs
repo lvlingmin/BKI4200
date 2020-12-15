9816e056fa964df6854dee5d2662651eaf3d9a33
2514817800@qq.com <2514817800@qq.com@8a603f74-7da7-47b3-bfc6-d9c48a895809>
2020-12-15 02:25:24 +0000

    修改一个返回的逻辑

--- branches/BioBaseCLIA(BKI4200)_理杯机/BioBaseCLIA/Run/frmTestResult.cs
+++ branches/BioBaseCLIA(BKI4200)_理杯机/BioBaseCLIA/Run/frmTestResult.cs
@@ -671,6 +671,7 @@ namespace BioBaseCLIA.Run
                 frmWorkList frmWL = (frmWorkList)Application.OpenForms["frmWorkList"];
                 frmWL.Show();
                 frmWL.BringToFront();
+                return;
             }
             this.Close();
         }
