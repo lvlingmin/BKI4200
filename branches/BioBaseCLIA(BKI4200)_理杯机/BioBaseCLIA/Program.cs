37a82e14897e9cbd67730c8cb9448c1b0d49b2fc
2514817800@qq.com <2514817800@qq.com@8a603f74-7da7-47b3-bfc6-d9c48a895809>
2021-02-24 06:31:42 +0000

    提交一个progran类获取提示信息方式  提交一个项目文件

--- branches/BioBaseCLIA(BKI4200)_理杯机/BioBaseCLIA/Program.cs
+++ branches/BioBaseCLIA(BKI4200)_理杯机/BioBaseCLIA/Program.cs
@@ -6,6 +6,8 @@ using BioBaseCLIA.User;
 using Localization;
 using System.IO;
 using System.Diagnostics;
+using Res = BioBaseCLIA.Resources.String.Program;
+using Common;
 //using BioBaseCLIA.Run;
 
 
@@ -22,15 +24,17 @@ namespace BioBaseCLIA
             //为程序的入口处增加捕获全局异常的功能，增加的内容包括try catch/以及三个捕获异常的方法，并会把信息写到文件中 jun add 20190328
             try
             {
-                if ((Process.GetProcesses().Where(x => x.ProcessName == Process.GetCurrentProcess().ProcessName).Select(x => x).ToList().Count > 1))
-                {
-                    MessageBox.Show("程序正在运行，请勿重复启动!", "温馨提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-                Language.AppCultureInfo = new System.Globalization.CultureInfo("zh-CN");
+                Language.AppCultureInfo = new System.Globalization.CultureInfo(GetCultureInfo());
                 //Language.AppCultureInfo = new System.Globalization.CultureInfo("en-US");
                 System.Threading.Thread.CurrentThread.CurrentCulture = Language.AppCultureInfo;
                 System.Threading.Thread.CurrentThread.CurrentUICulture = Language.AppCultureInfo;
+
+                if ((Process.GetProcesses().Where(x => x.ProcessName == Process.GetCurrentProcess().ProcessName).Select(x => x).ToList().Count > 1))
+                {
+                    MessageBox.Show(Res.Running, Res.Tips, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                
                 LanguageManager.LanguageName = Localization.Language.getLanName();//当前语言
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
@@ -57,18 +61,19 @@ namespace BioBaseCLIA
             catch (Exception ex) 
             {
                 string str = "";
-                string strDateInfo = "出现应用程序未处理的异常：" + DateTime.Now.ToString() + "\r\n";
+                string strDateInfo = Res.UnhandledException+": " + DateTime.Now.ToString() + "\r\n";
                 if (ex != null)
                 {
-                    str = string.Format(strDateInfo + "异常类型：{0}\r\n异常消息：{1}\r\n异常信息：{2}\r\n",
+                    str = string.Format(strDateInfo + Res.ExceptionType + 
+                        "{0}\r\n"+Res.ExceptionMsg+":{1}\r\n"+Res.ExceptionInfo+":{2}\r\n",
                          ex.GetType().Name, ex.Message, ex.StackTrace);
                 }
                 else
                 {
-                    str = string.Format("应用程序线程错误:{0}", ex);
+                    str = string.Format(Res.ExceptionInfo+"{0}", ex);
                 }
                 writeLog(str);
-                MessageBox.Show("发生系统错误，请及时联系技术人员！", "系统错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show(Res.SystemException, Res.Tips, MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
         /// <summary>
@@ -80,10 +85,10 @@ namespace BioBaseCLIA
         {
             string str = "";
             Exception error = e.ExceptionObject as Exception;
-            string strDateInfo = "出现应用程序未处理的异常：" + DateTime.Now.ToString() + "\r\n";
+            string strDateInfo =Res.UnhandledException+ "：" + DateTime.Now.ToString() + "\r\n";
             if (error != null)
             {
-                str = string.Format(strDateInfo + "Application UnhandledException:{0};\n\r堆栈信息:{1}", error.Message, error.StackTrace);
+                str = string.Format(strDateInfo + "Application UnhandledException:{0};\n\r"+Res.ExceptionInfo+":{1}", error.Message, error.StackTrace);
             }
             else
             {
@@ -91,24 +96,24 @@ namespace BioBaseCLIA
             }
 
             writeLog(str);
-            MessageBox.Show("发生系统错误，请及时联系技术人员！", "系统错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            MessageBox.Show(Res.SystemException , Res.Tips, MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
         static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
         {
             string str = "";
-            string strDateInfo = "出现应用程序未处理的异常：" + DateTime.Now.ToString() + "\r\n";
+            string strDateInfo =Res.UnhandledException +"：" + DateTime.Now.ToString() + "\r\n";
             Exception error = e.Exception as Exception;
             if (error != null)
             {
-                str = string.Format(strDateInfo + "异常类型：{0}\r\n异常消息：{1}\r\n异常信息：{2}\r\n",
+                str = string.Format(strDateInfo +Res.ExceptionType +"：{0}\r\n"+Res.ExceptionMsg+"：{1}\r\n"+Res.ExceptionInfo+"：{2}\r\n",
                      error.GetType().Name, error.Message, error.StackTrace);
             }
             else
             {
-                str = string.Format("应用程序线程错误:{0}", e);
+                str = string.Format(Res.ExceptionInfo+":{0}", e);
             }
             writeLog(str);
-            MessageBox.Show("发生系统错误，请及时联系技术人员！", "系统错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            MessageBox.Show(Res.SystemException,Res.Tips , MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
         /// <summary>
@@ -128,6 +133,16 @@ namespace BioBaseCLIA
                 sw.WriteLine("************************************************************************\n");
                 sw.Close();
             }
+        }
+
+        private static string GetCultureInfo()
+        {
+            if (OperateIniFile.ReadInIPara("CultureInfo", "Culture") == "en")
+            {
+                return "en";
+            }
+
+            return "zh-CN";
         }
     }
 }
