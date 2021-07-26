7915a0521a94632b9a1aa5bc17d527af5eb037d2
751414166@qq.com <751414166@qq.com@8a603f74-7da7-47b3-bfc6-d9c48a895809>
2021-07-26 08:13:29 +0000

    重新提交

---
+++ branches/BioBaseCLIA(BKI4200)_理杯机-15英寸一体机/BioBaseCLIA/frmVersion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BioBaseCLIA
+{
+    public partial class frmVersion : frmSmallParent 
+    {
+        //AssemblyInformation AssemblyInfor = new AssemblyInformation();
+        public frmVersion()
+        {
+            this.ControlBox = false;
+            InitializeComponent();
+            //label8.Text = AssemblyInfor.AssemblyCompany;
+        }
+        
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
---
+++ trunk/BioBaseCLIA(网口通讯)/BioBaseCLIA/Run/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioBaseCLIA.Run
+{
+    class Class1
+    {
+    }
+}
